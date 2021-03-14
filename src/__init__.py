159047d94b938dc936c23e7a4d02bc87bb61fefe
pnowk <pnowk123@gmail.com>
2021-03-14 12:06:05 +0100

    port change

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,17 @@
 
 
+from socketserver import BaseRequestHandler, TCPServer
+
+class Handler(BaseRequestHandler):
+    def handle(self):
+        print('get connection from', self.client_address)
+        while True:
+            msg = self.request.recv(8192)
+            if not msg:
+                break
+            self.request.send(msg)
 
 
-print('gutter'.center(79, '*'))
+if __name__ == "__main__":
+    serv = TCPServer(('', 7777), Handler)
+    serv.serve_forever()
